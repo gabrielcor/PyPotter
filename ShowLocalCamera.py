2a84a471880fcfd77aefbabbebf225e6dc81ecdf
Gabriel Cor <gabrielcor@hotmail.com>
2024-03-03 08:35:08 -0300

    add parameter to showlocal camera

--- ShowLocalCamera.py
+++ ShowLocalCamera.py
@@ -1,7 +1,16 @@
 import cv2
 import numpy as np
-#Camera
-cap = cv2.VideoCapture(0)
+import sys
+
+print(len(sys.argv))
+# Check for required number of arguments
+if (len(sys.argv) < 2):
+    #Camera
+    cap = cv2.VideoCapture(0)
+else:
+    cap = cv2.VideoCapture(sys.argv[1])
+
+
 
 while(1):
     res, frame = cap.read()
